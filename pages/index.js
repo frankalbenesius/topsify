33a6215a1ee5a8b9b91e63c751d73710e00600de
Frank Albenesius <frankalbenesius@gmail.com>
2017-02-07 22:01:14 -0600

    implicit grant authorization set up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+/* global window */
 import React from 'react'
+import { authorizeHref, getToken } from '../modules/spotify'
 
-export default () => (
-  <h1>TOPSIFY</h1>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      authorized: false,
+    }
+  }
+  componentDidMount() {
+    const token = getToken(window.location.hash)
+    if (token) {
+      this.setState({ // eslint-disable-line
+        authorized: true,
+        token,
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>TOPSIFY</h1>
+        {this.state.authorized ? (
+          <p>Nice, authorized</p>
+        ) : (
+          <a href={authorizeHref}>Authorize With Spotify</a>
+        )}
+      </div>
+    )
+  }
+}
+
+export default IndexPage
